c311a71bfe1f0dfe8a6c992da37a2595af717718
Martin710 <82586548+Martin710@users.noreply.github.com>
2026-08-29 01:08:47 +0800

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // unordered_map<string, vector<string>> groups;
+        // for(const auto& s : strs){
+        //     string key = s;
+        //     sort(key.begin(), key.end());
+        //     groups[key].push_back(s);
+        // }
+
+        // vector<vector<string>> result;
+
+        // for(auto& entry : groups){
+        //     result.push_back(entry.second);
+        // }
+        // return result;
+        unordered_map<string, vector<string>> groups;
+
+        for(const auto& s : strs){
+            string key = s;
+            sort(key.begin(), key.end());//不用k = ...
+            groups[key].push_back(s);
+        }
+        vector<vector<string>> results;
+
+        for(auto& entry : groups){
+            results.push_back(entry.second);//.second()X no ()
+        }
+
+        return results;
+
+    }
+};
