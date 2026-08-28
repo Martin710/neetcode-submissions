3235766a70e1d857fac12fa92c00c58795d5dacd
Martin710 <82586548+Martin710@users.noreply.github.com>
2026-08-29 00:51:12 +0800

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // unordered_map <int, int> hash;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //    int complement = target - nums[i];
+        //    if(hash.count(complement)){
+        //     return{hash[complement], i};
+        //    }
+        //    hash[nums[i]] = i;
+        // }
+        // return{};
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++){
+            if(map.count(target - nums[i])){
+                return {map[target - nums[i]], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
