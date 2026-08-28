a92b16902653ae6671417e0907b4b1e9a9bc84cc
Martin710 <82586548+Martin710@users.noreply.github.com>
2026-08-28 19:46:33 +0800

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int current = 0;
+        int max_value = INT_MIN;
+        for(int i = 0; i < nums.size(); i++){
+            if(current < 0){
+                current = nums[i];
+            }
+            else{
+                current+=nums[i];
+            }
+            
+            max_value = max(max_value, current);
+        }
+
+        return max_value;
+    }
+};
