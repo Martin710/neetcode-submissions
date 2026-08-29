1b926379e87d6ebb66856cd5fadd4bab30a4ea60
Martin710 <82586548+Martin710@users.noreply.github.com>
2026-08-29 17:57:02 +0800

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for (auto& p : prerequisites) {
+            int a = p[0], b = p[1];
+            graph[b].push_back(a);
+            indegree[a]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0) q.push(i);
+
+        int finished = 0;
+        while (!q.empty()) {
+            int course = q.front(); q.pop();
+            finished++;
+            for (int next : graph[course]) {
+                indegree[next]--;
+                if (indegree[next] == 0) q.push(next);
+            }
+        }
+        return finished == numCourses;
+    }
+};
+
+ 
